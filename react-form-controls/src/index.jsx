71082499223fb0f432545aeaf9093e1c16161686
Fernando Figueiredo <bookaneers@gmail.com>
2022-02-17 11:29:24 -0700

    Completed react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NewsletterForm  from './form-control';
+
+ReactDOM.render(
+    <NewsletterForm />,
+    document.querySelector('#root')
+);
