ff397b0ab092d7000245c657b963239838bff547
Fernando Figueiredo <bookaneers@gmail.com>
2022-02-17 10:17:17 -0700

    Finally completed

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import Watch from './watch';
-
-ReactDOM.render(
-  <Watch/>,
-  document.querySelector('#root')
-=======
 import Stopwatch from './stopwatch';
 
 ReactDOM.render(
     <Stopwatch />,
     document.querySelector('#root')
->>>>>>> 764c9c8 (Completed react-stopwatch-component)
 );
