7857b5f79a72ed33ab2005fa74063ea63851b79a
Fernando Figueiredo <bookaneers@gmail.com>
2022-02-17 16:01:50 -0700

    Completed react-validated-input-component

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Validation  from './validated-input';
+
+ReactDOM.render(
+    <Validation />,
+    document.querySelector('#root')
+);
