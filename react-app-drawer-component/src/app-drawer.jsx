c9bcd5ddcaca38740b5709f0d85b6584e2107006
Fernando Figueiredo <bookaneers@gmail.com>
2022-02-18 17:25:58 -0700

    Halfway done with react-app-drawer-component

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class AppDrawer  extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menu: 'on',
+            modal: 'on'
+        };
+        this.handleClickMenu = this.handleClickMenu.bind(this);
+    }
+
+    handleClickMenu(event) {
+        console.log('event ', event)
+        if (this.state.menu === 'on') { 
+            console.log('click OFF')
+            this.setState({ menu: 'off' }); 
+            this.setState({ modal: 'off' }); 
+        } else { 
+            console.log('click ON')
+            this.setState({ menu: 'on' }); 
+            this.setState({ modal: 'on' }); 
+        }
+    }
+
+    render() {
+        return (
+            <div className='main'> 
+                <div className={`modal ${this.state.modal}`}>
+                    <img class='menu' onClick={this.handleClickMenu} 
+                    className={`menu ${this.state.menu}`} src='./images/menu.png'/> 
+                                      
+                </div>
+                <img class='devil'src='./images/devil.png'/>  
+            </div>
+        );
+    }
+  }
+
+export default AppDrawer ;
