e2540145a05ca891ad38bf0bf7d13d06f22b0e2b
Fernando Figueiredo <bookaneers@gmail.com>
2022-02-23 12:44:15 -0700

    Complted react-app-drawer-component

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -5,37 +5,52 @@ class AppDrawer  extends React.Component {
         super(props);
         this.state = {
             menu: 'on',
-            modal: 'on'
+            modal: 'off'
         };
         this.handleClickMenu = this.handleClickMenu.bind(this);
     }
 
     handleClickMenu(event) {
-        console.log('event ', event)
+        // console.log('event ', event)
         if (this.state.menu === 'on') { 
             console.log('click OFF')
             this.setState({ menu: 'off' }); 
-            this.setState({ modal: 'off' }); 
+            this.setState({ modal: 'on' }); 
         } else { 
             console.log('click ON')
             this.setState({ menu: 'on' }); 
-            this.setState({ modal: 'on' }); 
+            this.setState({ modal: 'off' }); 
         }
     }
 
     render() {
-        return (
-            <div className='main'> 
-                <div className={`modal ${this.state.modal}`}>
-                    
-                    <img class='menu' onClick={this.handleClickMenu} 
-                    className={`menu ${this.state.menu}`} src='./images/menu.png'/> 
-                                      
+        if (this.state.menu === 'on') {
+            return (
+                <div className='main'> 
+                    <img class='dragonfly'src='./images/dragonfly.png'/> 
+                    <div className={`menu ${this.state.menu}`}>
+                        <img className='menuBtn' onClick={this.handleClickMenu} src='./images/menu.png'/> 
+                    </div>    
+                    <div className={`modal ${this.state.modal}`}>  
+                    </div>
                 </div>
-                <img class='dragonfly'src='./images/dragonfly.png'/>  
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div className='main' onClick={this.handleClickMenu}> 
+                    <img class='dragonfly'src='./images/dragonfly.png'/> 
+                    <div className={`menu ${this.state.menu}`}>
+                        <h1>Menu</h1>
+                        <h3>About Us</h3> 
+                        <h3>Get Started</h3> 
+                        <h3>Sign In</h3> 
+                    </div>    
+                    <div className={`modal ${this.state.modal}`}>  
+                    </div>
+                </div>
+            );
+        }
     }
-  }
+}
 
 export default AppDrawer ;
