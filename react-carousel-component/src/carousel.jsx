1b6030b65043cdef774c753610068105189bbf18
Fernando Figueiredo <bookaneers@gmail.com>
2022-02-24 22:01:01 -0700

    Missing Dots

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -2,70 +2,77 @@ import React from 'react';
 
 class Carousel extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            openId: 0
-        };
-        this.handleClickRight = this.handleClickRight.bind(this)
-        this.handleClickLeft = this.handleClickLeft.bind(this)
+    this.state = {
+      openId: 0
+    };
+    // this.handleClickRight = this.handleClickRight.bind(this);
+    // this.handleClickLeft = this.handleClickLeft.bind(this);
+    this.counting();
+  }
+
+  counting() {
+    this.timerID = setInterval(
+      () => this.tick(), 3000);
+  }
+
+  tick() {
+    if (this.state.openId === 4) {
+      this.setState({ openId: 0 });
+    } else {
+      this.setState({ openId: this.state.openId + 1 });
     }
+  }
 
-    handleClickRight() {
-        if (this.state.openId === 4) {
-            this.setState({openId: 0})
-        } else {
-            this.setState({openId: this.state.openId + 1})
-        }
-        console.log('this.state.openId : ', this.state.openId);
+  handleClickRight() {
+    if (this.state.openId === 4) {
+      this.setState({ openId: 0 });
+    } else {
+      this.setState({ openId: this.state.openId + 1 });
     }
+  }
 
-    handleClickLeft() {
-        if (this.state.openId === 0) {
-            this.setState({openId: 4})
-        } else {
-            this.setState({openId: this.state.openId - 1})
-        }
-        console.log('this.state.openId : ', this.state.openId);
+  handleClickLeft() {
+    if (this.state.openId === 0) {
+      this.setState({ openId: 4 });
+    } else {
+      this.setState({ openId: this.state.openId - 1 });
     }
+  }
 
-    render() {
+  getDots() {
+    return (
+      <div className='dots'>
+        <i className="fas fa-circle"></i>
+        <i className="far fa-circle"></i>
+        <i className="far fa-circle"></i>
+        <i className="far fa-circle"></i>
+        <i className="far fa-circle"></i>
+      </div>
+    );
+  }
 
-        console.log('STATE', this.state);
-        console.log('PROPS', this.props);
-        console.log('PROPS', this.props.items[0]);
+  render() {
+    return (
+      <div className='carousel'>
+          <div className='iconsandarrows'>
+              <div className='leftarrow' onClick={() => this.handleClickLeft()}>
+                  <i className='fas fa-angle-left' />
+              </div>
+              <div className='icons'>
+                  <img src = {this.props.items[this.state.openId].url} />
+              </div>
+              <div className='rightarrow' onClick={() => this.handleClickRight()}>
+                  <i className='fas fa-angle-right' />
+              </div>
+          </div>
 
-        return (
-            <div className='carousel'>
-                <div className='iconsandarrows'>
-
-                    <div className='leftarrow' onClick={() => this.handleClickLeft()}>
-                        <i className='fas fa-angle-left' />
-                    </div>
-                    <div className='icons'>
-                        <h3>{this.props.items[this.state.openId].url}</h3>
-                    </div>
-                    <div className='rightarrow' onClick={() => this.handleClickRight()}>
-                        <i className='fas fa-angle-right' />
-                    </div>
-
-                </div>
-
-                
-                <div className='dots'>
-                <i className="fas fa-circle"></i>
-                <i className="far fa-circle"></i>
-                <i className="far fa-circle"></i>
-                <i className="far fa-circle"></i>
-                <i className="far fa-circle"></i>
-                </div> 
-
-            </div>
-
-        );
-
-    }
+          { this.getDots()}
+      </div>
+    );
+  }
 }
 
 export default Carousel;
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Carousel from './carousel'
+import Carousel from './carousel';
 
 const items = [
-  {
-      url: 'one'
-  },
-  {
-      url: 'two'
-  },
-  {
-      url: 'three'
-  },
-  {
-      url: 'four'
-  },
-  {
-      url: 'five'
-  }
-]
+  { id: 1, url: '../dist/images/001.png' },
+  { id: 2, url: '../dist/images/004.png' },
+  { id: 3, url: '../dist/images/007.png' },
+  { id: 4, url: '../dist/images/025.png' },
+  { id: 5, url: '../dist/images/039.png' }
+];
 
 ReactDOM.render(
     <Carousel items={items} />,
