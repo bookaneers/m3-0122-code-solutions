8c57b1f2af71ec6cb7731d9ebb17dd507dc2d380
Fernando Figueiredo <bookaneers@gmail.com>
2022-02-28 09:24:45 -0700

    Completed express-static

--- express-static/index.js
+++ express-static/index.js
@@ -5,15 +5,13 @@ const path = require('path');
 const pathJoin = (path.join(__dirname, 'public'));
 const statFiles = app.use(express.static(pathJoin));
 
-console.log('path: ', path);
-console.log('pathJoin: ', pathJoin);
-console.log('statFiles: ', statFiles);
+console.log(statFiles);
 
 app.use('/', (req, res, next) => {
-    console.log(req.method);
-    res.send('hello hello');
-    next();
-})
+  console.log(req.method);
+  res.send('welcome to express-static');
+  next();
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
