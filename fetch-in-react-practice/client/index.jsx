a3b897001d56c86e2754df4c9bd8bb9a759a1bb0
Fernando Figueiredo <bookaneers@gmail.com>
2022-03-07 16:03:50 -0700

    Updated fetch-in-react-practice

--- fetch-in-react-practice/client/index.jsx
+++ fetch-in-react-practice/client/index.jsx
@@ -1,7 +1,9 @@
+// define dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+// render method
 ReactDOM.render(
   <App/>,
   document.querySelector('#root')
